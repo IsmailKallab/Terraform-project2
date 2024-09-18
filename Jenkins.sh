33bc4c1af0cb96901c960578150de17ee8211ee6
ubuntu <ubu@tu.com>
2024-09-18 16:48:24 +0300

    changing jenkins.sh file for instaltion

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,13 +1,11 @@
 # visit the https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/#installing-and-configuring-jenkins
 #!/bin/bash
 sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo dnf install java-17-amazon-corretto -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
+sudo apt install temurin-17-jdk -y
+/usr/bin/java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
 sudo systemctl start jenkins
+sudo systemctl status jenkins
